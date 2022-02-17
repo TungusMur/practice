9d98022743a20a1f80798e061ac2e49d9e4008ef
TungusMur <vova.myraw@gmail.com>
2022-02-17 17:02:08 +0400

    V-01 создание макета

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,9 +1,15 @@
+import Header from '../Header';
+import Main from '../Main';
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello world</h1>
+    <div className="app">
+      <Router>
+        <Header />
+        <Main />
+      </Router>
     </div>
   );
 };
---
+++ src/components/PrimaryMain/index.jsx
@@ -0,0 +1,3 @@
+import PrimaryMain from './PrimaryMain';
+
+export default PrimaryMain;
