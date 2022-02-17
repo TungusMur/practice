2644dd2dbcf9d091809259c8a0bd3e5257afa87f
TungusMur <vova.myraw@gmail.com>
2022-02-17 22:51:54 +0400

    настройка окружения

---
+++ src/assets/img/Slider/imgList.jsx
@@ -0,0 +1,17 @@
+import sliderImg1 from './sliderImg1.jpg';
+import sliderImg2 from './sliderImg2.jpg';
+import sliderImg3 from './sliderImg3.jpg';
+import sliderImg4 from './sliderImg4.jpg';
+
+export default [
+  {
+    id: 0,
+    img: sliderImg1,
+    chapter: 'Бесплатный парковка',
+    description:
+      'Оставляйте машину на платных городских парковках и разрешенных местах, не нарушая ПДД, а также в аэропортах. ',
+  },
+  { id: 1, img: sliderImg2, chapter: 'Страховка', description: 'Полная страховка страховка автомобиля ' },
+  { id: 2, img: sliderImg3, chapter: 'Бензин', description: 'Полный бак на любой заправке города за наш счёт' },
+  { id: 3, img: sliderImg4, chapter: 'Обслуживание', description: 'Автомобиль проходит еженедельное ТО' },
+];
