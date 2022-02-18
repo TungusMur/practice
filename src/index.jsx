313de3d8da6f9a2490bf79e3e4627216a96c62ca
TungusMur <68353612+TungusMur@users.noreply.github.com>
2022-02-18 09:44:12 +0400

    Hotfix F-01 (#5)
    * F-01 разработка слайдера
    
    * F-01 изменение слайдера и добавление дизайна
    
    * F-01 добавление таймера и изменение слайдера
    
    * F-01 оптимизация картинок
    
    * F-01 добавление функционала
    
    * F-01 конец первых поправок

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import "./index.scss";
+import './assets/styles/reset.scss';
+import './assets/styles/fonts.scss';
 
 ReactDOM.render(<App />, document.getElementById("root"));
