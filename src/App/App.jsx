c10652e2d933a031c168a15a517711d6bde16e0b
TungusMur <68353612+TungusMur@users.noreply.github.com>
2022-02-18 04:39:13 +0400

    Hotfix f 01 (#3)
    * F-01 разработка слайдера
    
    * F-01 изменение слайдера и добавление дизайна
    
    * F-01 добавление таймера и изменение слайдера
    
    * F-01 оптимизация картинок
    
    * F-01 добавление функционала

--- src/App/App.jsx
+++ src/App/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Slider from '../components/Slider';
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello world</h1>
+    <div className="app">
+      <Slider />
     </div>
   );
 };
