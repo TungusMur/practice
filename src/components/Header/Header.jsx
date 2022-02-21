f36441a9cfab786ef33a1f014d41a1fdde85dd5c
TungusMur <vova.myraw@gmail.com>
2022-02-21 13:50:52 +0400

    V-01 добавление адаптива

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,28 +1,53 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState, useCallback } from 'react';
 import { NavLink } from 'react-router-dom';
+import iconMap from '../../assets/img/Map/icon.svg';
 import facebook from '../../assets/img/SocialNetwork/facebook.svg';
+import facebookHover from '../../assets/img/SocialNetwork/facebookHover.svg';
 import instagram from '../../assets/img/SocialNetwork/instagram.svg';
+import instagramHover from '../../assets/img/SocialNetwork/instagramHover.svg';
 import telegram from '../../assets/img/SocialNetwork/telegram.svg';
+import telegramHover from '../../assets/img/SocialNetwork/telegramHover.svg';
 import './styles.scss';
 
 const Header = () => {
-  const [activeStatusHeaderActions, setActiveStatusHeaderActions] = useState('');
+  const [statusHeaderClick, setStatusHeaderClick] = useState('');
+  const [statusHoverImg, setStatusHoverImg] = useState('');
+
+  useEffect(() => {
+    document.body.style.overflow = statusHeaderClick ? 'hidden' : 'auto';
+  }, [statusHeaderClick]);
+
   return (
-    <header className={`${activeStatusHeaderActions}`}>
-      <div className="header-form">
+    <header className={`${statusHeaderClick}`}>
+      {/* <div className="header-primary">
+        <div className="header-primary-form">
+          <div className="header-primary-content">
+            <div className="header-primary__logo">
+              <NavLink className="header-primary_link" to="/">
+                <h4>Need for drive</h4>
+              </NavLink>
+            </div>
+            <div className="header-primary__map">
+              <img src={iconMap} alt="iconMap" />
+              <h5>Ульяновск</h5>
+            </div>
+          </div>
+        </div>
+      </div> */}
+      <div className="header-bar">
         <button
-          className={`header-form_button-burger button-burger ${activeStatusHeaderActions}`}
+          id="button-burger"
+          className={`header-bar_button-burger button-burger ${statusHeaderClick}`}
           onClick={() => {
-            activeStatusHeaderActions ? setActiveStatusHeaderActions('') : setActiveStatusHeaderActions('active');
-            console.log('asd');
+            statusHeaderClick ? setStatusHeaderClick('') : setStatusHeaderClick('active');
           }}
         >
           <span></span>
         </button>
-        <button className="header-form_button-changeLanguage">Eng</button>
+        <button className={`header-bar_button-changeLanguage ${statusHeaderClick}`}>Eng</button>
       </div>
-      <div className={`header-menu ${activeStatusHeaderActions}`}>
-        <div className={`header-menu-form ${activeStatusHeaderActions}`}>
+      <div className={`header-menu ${statusHeaderClick}`}>
+        <div className={`header-menu-form ${statusHeaderClick}`}>
           <div className="header-content">
             <div className="header-navigation">
               <div className="header-navigation_item">
@@ -48,19 +73,43 @@ const Header = () => {
             </div>
             <div className="header-socialNetwork">
               <div className="header-socialNetwork-form">
-                <div className="header-socialNetwork_item">
-                  <NavLink className="header-socialNetwork_link" to="#">
-                    <img src={telegram} alt="telegram" />
+                <div
+                  className="header-socialNetwork_item"
+                  onMouseOver={() => {
+                    setStatusHoverImg('telegram');
+                  }}
+                  onMouseOut={() => {
+                    setStatusHoverImg('');
+                  }}
+                >
+                  <NavLink className="header-socialNetwork_link" to="/telegram">
+                    <img src={statusHoverImg === 'telegram' ? telegramHover : telegram} alt="telegram" />
                   </NavLink>{' '}
                 </div>
-                <div className="header-socialNetwork_item">
-                  <NavLink className="header-socialNetwork_link" to="#">
-                    <img src={facebook} alt="facebook" />
+                <div
+                  className="header-socialNetwork_item"
+                  onMouseOver={() => {
+                    setStatusHoverImg('facebook');
+                  }}
+                  onMouseOut={() => {
+                    setStatusHoverImg('');
+                  }}
+                >
+                  <NavLink className="header-socialNetwork_link" to="/facebook">
+                    <img src={statusHoverImg === 'facebook' ? facebookHover : facebook} alt="facebook" />
                   </NavLink>
                 </div>
-                <div className="header-socialNetwork_item">
-                  <NavLink className="header-socialNetwork_link" to="#">
-                    <img src={instagram} alt="instagram" />
+                <div
+                  className="header-socialNetwork_item"
+                  onMouseOver={() => {
+                    setStatusHoverImg('instagram');
+                  }}
+                  onMouseOut={() => {
+                    setStatusHoverImg('');
+                  }}
+                >
+                  <NavLink className="header-socialNetwork_link" to="/instagram">
+                    <img src={statusHoverImg === 'instagram' ? instagramHover : instagram} alt="instagram" />
                   </NavLink>
                 </div>
               </div>
